12374f227ce16451063c560dccafa9294e12ae60
anousias-k3y <anousias@k3y.bg>
2026-01-28 10:40:12 +0200

    refactor: Add dummy method `clear` in progress classes

--- packages/binvis/src/binvis/progress.py
+++ packages/binvis/src/binvis/progress.py
@@ -2,7 +2,13 @@ class Progress:
     def __init__(self, *args, **kwargs):
         pass
 
+    def clear(self, *args, **kwargs):
+        pass
+
 
 class Dummy:
     def __init__(self, *args, **kwargs):
+        pass
+
+    def clear(self, *args, **kwargs):
         pass
